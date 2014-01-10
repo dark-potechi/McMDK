4e3a1e947715f07d25e9ed7bd1089937f476dfaf
tuyapin <tuyapin0130@gmail.com>
2014-01-11 03:33:28 +0900

    つらみ

---
+++ McMDK.Plugin/IPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McMDK.Plugin
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// プラグインの名前を取得します。
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// プラグインのバージョンを取得します。
+        /// </summary>
+        string Version { get; }
+
+        /// <summary>
+        /// プラグインロード時に呼び出されます。
+        /// </summary>
+        void Initialize();
+
+    }
+}
---
+++ McMDK/Define.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McMDK
+{
+    public class Define
+    {
+        /// <summary>
+        /// 実行ファイルパス
+        /// </summary>
+        public static string FilePath
+        {
+            get
+            {
+                return Process.GetCurrentProcess().MainModule.FileName;
+            }
+        }
+
+        public static string GetVersion()
+        {
+            return Version + "." + Release;
+        }
+
+        public static readonly string Version = "2.0.0";
+
+        public static readonly int Release = 25;
+
+        public static readonly string PluginDirectory = "plugins";
+
+        public static readonly string ProjectDirectory = "projects";
+
+        public static readonly string ProtectPass = "Mi8dEppKhXck95rgmNfyc3AXd";
+
+        public static readonly string NewVersionUrl = "http://tuyapin.net/mcmdk/application/version.xml";
+
+        public static readonly string MinecraftForgeUrl = "http://files.minecraftforge.net/minecraftforge/minecraftforge-src-{0}-{1}.zip";
+
+        public static readonly string ForgeGradleUrl = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/{0}-{1}/forge-{0}-{1}-src.zip";
+
+        public static readonly string CoderPackUrl = "http://mcp.ocean-labs.de/files/archive/mcp{0}.zip";
+
+        public static readonly string MinecraftLoginUrl = "https://login.minecraft.net/?user={0}&password={1}&version=14";
+
+
+    }
+}
